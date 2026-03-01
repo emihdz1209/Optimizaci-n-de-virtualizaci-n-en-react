1052149a811a70b5e79f5c91cc4f95478d8845a6
emihdz1209 <emiliohdz35@gmail.com>
2026-03-01 15:42:48 -0600

    Textos com memo

---
+++ src/components/counter/TextoC.jsx
@@ -0,0 +1,14 @@
+import { memo } from "react";
+
+function TextoC() {
+  return (
+    console.log("Renderizando TextoC"),
+    (
+      <div className="texto-hijo">
+        <p>Soy el componente TextoC</p>
+      </div>
+    )
+  );
+}
+
+export default memo(TextoC);
